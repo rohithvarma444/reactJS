0087e8486a02761a6e92789d4ff519b41c99b482
rohithvarma444 <rohithvarmadatla12@gmail.com>
2024-04-09 15:49:46 +0530

    adding incremental upcount and downcount

--- state-react/src/App.jsx
+++ state-react/src/App.jsx
@@ -10,13 +10,24 @@ function App() {
   const decrement = () => {
     setCount(counter -1);
   }
+  const addbyTwo = () => {
+    setCount((prevNum)=> prevNum+1);
+    setCount((prevNum)=> prevNum+1);
+  }
+  const decbyTwo = () => {
+    setCount((prevNum)=> prevNum-1);
+    setCount((prevNum)=> prevNum-1);
+  }
   const username = 'Rohith Varma';
   return (
     <>
       <h1>Learning React useState() <br /> by <i>{username}</i></h1>
       <h2>The value of the counter is {counter}</h2>
-      <button onClick={add} >Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <button onClick={add} >Increment{" "}</button>
+      <button onClick={decrement}>Decrement{" "}</button>
+      <br /><br />
+      <button onClick={addbyTwo}>IncrementByTwo</button>
+      <button onClick={decbyTwo}>DecrementByTwo</button>
     </>
   )
 }
